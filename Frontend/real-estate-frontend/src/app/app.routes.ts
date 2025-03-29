e2c446f267f83e9fdcbde3bff3fef328b50219f5
linaehab <linaehab149@gmail.com>
2025-03-29 14:01:05 +0200

    frontend home

--- Frontend/real-estate-frontend/src/app/app.routes.ts
+++ Frontend/real-estate-frontend/src/app/app.routes.ts
@@ -3,12 +3,13 @@ import { LoginComponent } from './auth/login/login.component';
 import { SignUpComponent } from './auth/sign-up/sign-up.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ForgotPasswordComponent } from './auth/forgot-password/forgot-password.component'; 
-
+import { HomeComponent } from './components/home/home.component';
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' }, 
   { path: 'login', component: LoginComponent },
   { path: 'sign-up', component: SignUpComponent },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'forgot-password', component: ForgotPasswordComponent },
+  {path: 'home', component: HomeComponent},
 
 ];
---
+++ Frontend/real-estate-frontend/src/app/components/property-cards/property-cards.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-property-cards',
+  imports: [],
+  templateUrl: './property-cards.component.html',
+  styleUrl: './property-cards.component.css'
+})
+export class PropertyCardsComponent {
+
+}
