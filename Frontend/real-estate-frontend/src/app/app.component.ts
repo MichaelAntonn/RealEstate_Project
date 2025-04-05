94fd753b5f1f8dceabba0c861b8c96a4ece25a38
ahmed hidar <ahmedheedar@gmail.com>
2025-04-05 23:48:15 +0200

    fix error in app.component.ts

--- Frontend/real-estate-frontend/src/app/app.component.ts
+++ Frontend/real-estate-frontend/src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import { bootstrapApplication } from '@angular/platform-browser';
-import { provideRouter } from '@angular/router';
-import { routes } from './app.routes';
+
+
 
 @Component({
   selector: 'app-root',
