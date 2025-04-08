754516511e9ef7f9dee5ff7ada1eb3e71690b55b
linaehab <linaehab149@gmail.com>
2025-04-08 02:33:52 +0200

    Bugs fixed

--- Frontend/real-estate-frontend/src/app/user-dashboard/models/appointment.model.ts
+++
@@ -1,9 +0,0 @@
-export interface Appointment {
-  id: number;
-  date: string;
-  time: string;
-  client: string;
-  property: string;
-  purpose: string;
-  status: 'Scheduled' | 'Confirmed' | 'Pending' | 'Cancelled';
-}
