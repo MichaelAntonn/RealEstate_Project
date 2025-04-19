d9dc64471af3abef50d342eea58857cd81637918
norhan-magdy <norhanmagdy638@gmail.com>
2025-04-19 02:20:56 +0200

    property details with backend implementation

--- Frontend/real-estate-frontend/src/app/components/property-details/property-details.component.ts
+++ Frontend/real-estate-frontend/src/app/components/property-details/property-details.component.ts
@@ -1,14 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PropertyService } from '../../services/property.service';
+import { Property } from '../../models/property';
 import { AddressSectionComponent } from '../address-section/address-section.component';
-import { DetailsSectionComponent } from "../details-section/details-section.component";
-import { DetailsRightSectionComponent } from "../details-right-section/details-right-section.component";
-import { FooterComponent } from "../footer/footer.component";
-import { NavbarComponent } from "../navbar/navbar.component";
+import { DetailsSectionComponent } from '../details-section/details-section.component';
+import { DetailsRightSectionComponent } from '../details-right-section/details-right-section.component';
+import { FooterComponent } from '../footer/footer.component';
+import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-property-details',
-  imports: [AddressSectionComponent, DetailsRightSectionComponent, DetailsSectionComponent, FooterComponent, NavbarComponent],
+  imports: [
+    AddressSectionComponent,
+    DetailsRightSectionComponent,
+    DetailsSectionComponent,
+    FooterComponent,
+    NavbarComponent,
+  ],
   templateUrl: './property-details.component.html',
-  styleUrl: './property-details.component.css',
+  styleUrls: ['./property-details.component.css'],
 })
-export class PropertyDetailsComponent { }
+export class PropertyDetailsComponent implements OnInit {
+  propertyId: number | null = null;
+
+  constructor(
+    private route: ActivatedRoute,
+    private propertyService: PropertyService
+  ) {}
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((params) => {
+      const slug = params.get('slug');
+      if (slug) {
+        this.propertyService.getPropertyBySlug(slug).subscribe({
+          next: (property: Property) => {
+            this.propertyId = property.id;
+          },
+          error: (error) => {
+            this.propertyId = null;
+          },
+        });
+      } else {
+        this.propertyId = null;
+      }
+    });
+  }
+}
