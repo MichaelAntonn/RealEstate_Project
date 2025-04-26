bc6363c4b2f1aedf13ccb564ab5564b3b4508236
ahmed hidar <ahmedheedar@gmail.com>
2025-04-26 09:23:22 +0300

    fix rent /month in property card

--- Frontend/real-estate-frontend/src/app/components/property-card/property-card.component.ts
+++ Frontend/real-estate-frontend/src/app/components/property-card/property-card.component.ts
@@ -13,8 +13,18 @@ import { RouterLink } from '@angular/router';
 export class PropertyCardComponent {
   @Input() properties: Property[] = [];
 
-  // Get the image URL for a property, with a fallback if no image is provided
   getImageUrl(property: Property): string {
-    return property?.cover_image || 'assets/6fbcaa7c-3dbe-494e-afdf-5896e2e2a090.webp';
+    return property.cover_image 
+      ? `http://127.0.0.1:8000/storage/${property.cover_image}` 
+      : 'assets/6fbcaa7c-3dbe-494e-afdf-5896e2e2a090.webp';
+  }
+
+  getPriceDisplay(property: Property): string {
+    if (property.listing_type === 'for_sale') {
+      return `$${property.price.toLocaleString()}`; // Added toLocaleString for better formatting
+    } else if (property.listing_type === 'for_rent') {
+      return `$${property.price.toLocaleString()}`; // Added toLocaleString for better formatting
+    }
+    return '';
   }
 }
