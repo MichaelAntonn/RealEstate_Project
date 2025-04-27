1fd4f6a01a6287671bdf873542b22fd999589a5c
MichaelAntonn <michaelanton999@gmail.com>
2025-04-27 21:23:54 +0300

    Maps added

--- Frontend/real-estate-frontend/src/app/components/address-section/address-section.component.ts
+++ Frontend/real-estate-frontend/src/app/components/address-section/address-section.component.ts
@@ -1,13 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { GoogleMap ,MapMarker} from '@angular/google-maps';
+import { CommonModule } from '@angular/common'; // استورد CommonModule
 
 @Component({
   selector: 'app-address-section',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, GoogleMap,MapMarker], // ضيف CommonModule و GoogleMap هنا
   templateUrl: './address-section.component.html',
   styleUrl: './address-section.component.css',
 })
-export class AddressSectionComponent {
-  address = 'Caior 1, Egypt';
-  code = '2365';
+export class AddressSectionComponent implements OnChanges {
+  @Input() property: any;
+  latitude: number | null = null;
+  longitude: number | null = null;
+  center: google.maps.LatLngLiteral | null = null;
+  zoom = 16;
+  markerOptions: google.maps.MarkerOptions = { draggable: false };
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['property'] && changes['property'].currentValue) {
+      this.latitude = parseFloat(this.property.latitude);
+      this.longitude = parseFloat(this.property.longitude);
+      this.center = {
+        lat: this.latitude,
+        lng: this.longitude,
+      };
+    } else {
+      this.center = null;
+    }
+  }
 }
