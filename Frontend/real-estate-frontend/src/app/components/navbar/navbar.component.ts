47b8f227d7a9a2c81a249ea1d7526e9cd827c104
galal2241 <ahmed2241.galal@gmail.com>
2025-04-28 17:12:35 +0300

    Edit Navbar

--- Frontend/real-estate-frontend/src/app/components/navbar/navbar.component.ts
+++ Frontend/real-estate-frontend/src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterModule } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink, CommonModule],
+  imports: [RouterLink, CommonModule ,RouterModule ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
